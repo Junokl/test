fe1d3895adb7b6362ba36811339412915a372385
hsj <992483331@qq.com>
2018-11-29 22:39:26 +0800

    2

---
+++ build/precache-manifest.f1d44b59896c7ef906ecb0f23eb0c328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af5f8effbd2099f06a3b7a126f39542d",
+    "url": "/static/media/sprite.af5f8eff.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "249ca443bfa9a187f27c",
+    "url": "/static/js/main.249ca443.chunk.js"
+  },
+  {
+    "revision": "3b4f1fad4373e9441d1f",
+    "url": "/static/js/1.3b4f1fad.chunk.js"
+  },
+  {
+    "revision": "249ca443bfa9a187f27c",
+    "url": "/static/css/main.95469d0e.chunk.css"
+  },
+  {
+    "revision": "07a562c774528199f87c902024c9ec68",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -11,9 +11,8 @@ import * as serviceWorker from './libs/serviceWorker';
 React.axios = axios;
 ReactDOM.render(
     <Router>
-        <App /> 
+        <App/> 
     </Router>,
-    
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
